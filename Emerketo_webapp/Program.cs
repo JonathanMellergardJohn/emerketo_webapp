9636d8ab735bc4c5e1b35204ec24cad11af8bbfc
Jonathan Mellergård John <jonathan.mellergard@gmail.com>
2023-05-24 19:38:41 +0200

    -added Razor Pages service and middleware -scaffolded Identity, Login and Registration

--- Emerketo_webapp/Program.cs
+++ Emerketo_webapp/Program.cs
@@ -1,7 +1,9 @@
 using Emerketo_webapp.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("EmerketoDbContextConnection") ?? throw new InvalidOperationException("Connection string 'EmerketoDbContextConnection' not found.");
 
 // SERVICE REGISTRATION
 // Services can be registered to the DI-container on variable builder
@@ -14,14 +16,16 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
     // deprecated and not recommended for use anymore.
 builder.Services.AddControllersWithViews();
 
-
+builder.Services.AddRazorPages();
 // Register DbContext
-
 builder.Services.AddDbContext<EmerketoDbContext>(options =>
 {
     options.UseSqlServer(
         builder.Configuration["ConnectionStrings:EmerketoDbContextConnection"]);
 });
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<EmerketoDbContext>();
 
 var app = builder.Build();
 
@@ -41,5 +45,6 @@ app.MapDefaultControllerRoute();
 
 // for seeding an empty database!
 DbInitializer.Seed(app);
+app.MapRazorPages();
 
 app.Run();
